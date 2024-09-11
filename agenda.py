d732bfb740af36ed27476da658533cdc86498a3d
ikidon <ikidon.cc@gmail.com>
2024-09-11 07:42:07 -0300

    allows you to update a contact

--- agenda.py
+++ agenda.py
@@ -1,4 +1,7 @@
-_DATABASE = []
+_DATABASE = [
+  { "name": "ikidon", "phone": "999999999", "email": "ikidon@gmail.com", "favorite": 0 },
+  { "name": "felipe", "phone": "111111111", "email": "felipe@gmail.com", "favorite": 0 }
+  ]
 
 def CreateContact():
   print("\n...New contact...\n")
@@ -23,6 +26,32 @@ def ListAllContacts():
   for contact in _DATABASE:
     print(contact)
 
+def UpdateContact():
+  print("\n...Update a contact...\n")
+
+  print("Yours contacts: ")
+  for contact in _DATABASE:
+    index = _DATABASE.index(contact)
+
+    print(f"{index} - {contact.get("name")}")
+
+  indexContact = int(input("\nWhich contact do you want to update? (Select by number on the right)\n> "))
+
+  contact = _DATABASE[indexContact]
+
+  print("\nLeave blank if you do not want to modify the current value.\n")
+  name = input("name: ",)
+  phone = input("phone: ")
+  email = input("email: ")
+  favorite = input("favorite (0 = No, 1 = Yes): ")
+
+  if(len(name) > 0): contact.update({ "name": name })
+  if(len(phone) > 0): contact.update({ "phone": phone })
+  if(len(email) > 0): contact.update({ "email": email })
+  if(favorite == 0 or favorite == 1): contact.update({ "favorite": favorite })
+
+  _DATABASE[indexContact].update(contact)
+
 def ShowOptions():
   options = (
     "0. Welcome to agenda!",
@@ -45,5 +74,6 @@ def Main():
 
   if(option_selected == 1): CreateContact()
   if(option_selected == 2): ListAllContacts()
+  if(option_selected == 3): UpdateContact()
 
 Main()
